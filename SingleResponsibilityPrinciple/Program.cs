80d83e01faae7fb11b14c0caa8b3f0a5ce76f58c
dbohn <dbohn@LSC-1086>
2016-11-26 00:10:24 -0600

    Request 407 - Added new class ReadURLTradeData. Class still requires debugging.

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -9,16 +9,16 @@ namespace SingleResponsibilityPrinciple
     {
         static void Main(string[] args)
         {
-            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
-
+            //var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
+            string url = "http://faculty.css.edu/tgibbons/trades4.txt";
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
-            var tradeDataProvider = new StreamTradeDataProvider(tradeStream);
+            //var tradeDataProvider = new StreamTradeDataProvider(tradeStream);
+            var urltradedata = new URLTradeDataProvider(url);
             var tradeMapper = new SimpleTradeMapper();
             var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
             var tradeStorage = new AdoNetTradeStorage(logger);
-
-            var tradeProcessor = new TradeProcessor(tradeDataProvider, tradeParser, tradeStorage);
+            var tradeProcessor = new TradeProcessor(urltradedata, tradeParser, tradeStorage);
             tradeProcessor.ProcessTrades();
 
             Console.ReadKey();
